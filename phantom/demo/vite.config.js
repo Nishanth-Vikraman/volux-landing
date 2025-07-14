27324e4da864fe3465d9d59002624b6a7c7c6752
S.Nishanth Vikraman <nishanth.vik@gmail.com>
2025-07-15 01:53:35 +0530

    Fix: Added base path and built React app with Vite

--- phantom/demo/vite.config.js
+++ phantom/demo/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/volux-landing/phantom/demo-deploy/',  // 👈 this is the key
+  base: '/phantom/demo-deploy/',  // 👈 this is the key
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
